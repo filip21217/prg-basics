6bae46cb747bd4e11544e66cd359fb128a315e21
filip21217 <filip.gosek1@gmail.com>
2025-10-23 11:12:55 +0200

    yrr

--- 02-TypesAndVariables/second.py
+++ 02-TypesAndVariables/second.py
@@ -1,1 +1,8 @@
-print("filip gosek")
+number1 = 21
+number2 = 22
+number3 = 23
+result = number1 + number2 + number3
+print('number1:', number1)
+print('number2:', number2)
+print('number3:', number3)
+print('result of summation:', result)
---
+++ 02-TypesAndVariables/swappingvarialbes.py
@@ -0,0 +1,5 @@
+x = 7
+y = 4
+z = 0
+print("Before swapping: x=", x, "y=", y)
+print("After swapping: x=", y, "y=", z)
